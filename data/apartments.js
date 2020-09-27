02645d489c121e7e388b4aa5ae47b86070e94380
Theresa <tsc82@cornell.edu>
2020-09-26 20:55:59 -0400

    Included dummy data in index.html

---
+++ data/apartments.js
@@ -0,0 +1,187 @@
+const apartments = [
+  {
+    "name": "301 College Avenue",
+    "location": "301 College Avenue, Ithaca, NY 14850",
+    "landlord": "Hollie Melucci (301 CA Associates, LLC)",
+    "image": "301collegeave",
+    "overall_rating": 4,
+    "landlord_rating": 4,
+    "tags_list": {
+      "amenities": 3,
+      "location": 3,
+      "laundry": 3
+    },
+    "reviews": [
+      {
+        "apartment_name": "301 College Avenue",
+        "apartment_unit": "3 BD",
+        "overall_rating": 4,
+        "landlord_rating": 5,
+        "tags_list": {
+          "amenities": 3,
+          "location": 3, 
+          "laundry": 2
+        },
+        "message": "Clean facilities and Hollie is very nice."
+      },
+      {
+        "apartment_name": "301 College Avenue",
+        "apartment_unit": "Studio",
+        "overall_rating": 4,
+        "landlord_rating": 3,
+        "tags_list": {
+          "amenities": 3,
+          "location": 3
+        },
+        "message": "Close to Greenstar and to campus."
+      },
+      {
+        "apartment_name": "301 College Avenue",
+        "apartment_unit": "2 BD",
+        "overall_rating": 4,
+        "landlord_rating": 5,
+        "tags_list": {
+          "amenities": 3,
+          "location": 3, 
+          "laundry": 2
+        },
+        "message": "The apartment is clean and it's just a 10 min walk from campus."
+      }
+    ]
+  },
+  {
+    "name": "Collegetown Plaza",
+    "location": "111 Dryden Road, Ithaca, NY 14850",
+    "landlord": "Ithaca Renting Company",
+    "image": "ctownplaza",
+    "overall_rating": 4,
+    "landlord_rating": 3,
+    "tags_list": {
+      "location": 3,
+      "laundry": 2,
+      "wifi": 2
+    },
+    "reviews": [
+      {
+        "apartment_name": "Collegetown Plaza",
+        "apartment_unit": "4 BD",
+        "overall_rating": 5,
+        "landlord_rating": 3,
+        "tags_list": {
+          "wifi": 2,
+          "location": 3, 
+          "laundry": 2
+        },
+        "message": "The location is great and the apartment is nice overall. Sometimes the landlord is late with maintenance calls."
+      },
+      {
+        "apartment_name": "Collegetown Plaza",
+        "apartment_unit": "3 BD",
+        "overall_rating": 3,
+        "landlord_rating": 3,
+        "tags_list": {
+          "location": 3
+        },
+        "message": "A good place for a ctown price."
+      }
+    ]
+  },
+  {
+    "name": "Collegetown Center",
+    "location": "151 Dryden Road, Ithaca, NY 14850",
+    "landlord": "Ithaca Renting Company",
+    "image": "ctowncenter",
+    "overall_rating": 5,
+    "landlord_rating": 3,
+    "tags_list": {
+      "location": 3,
+      "laundry": 3
+    },
+    "reviews": [
+      {
+        "apartment_name": "Collegetown Center",
+        "apartment_unit": "Studio",
+        "overall_rating": 5,
+        "landlord_rating": 3,
+        "tags_list": {
+          "location": 3, 
+          "laundry": 3
+        },
+        "message": "I'm renewing my lease because I like the place. Sometimes the landlord is hard to reach though."
+      }
+    ]
+  },
+  {
+    "name": "Collegetown Court",
+    "location": "208 Dryden Road, Ithaca, NY 14850",
+    "landlord": "Ithaca Renting Company",
+    "image": "ctowncourt",
+    "overall_rating": 3,
+    "landlord_rating": 3,
+    "tags_list": {
+      "noise": 3,
+      "price": 2
+    },
+    "reviews": [
+      {
+        "apartment_name": "Collegetown Court",
+        "apartment_unit": "3 BD",
+        "overall_rating": 4,
+        "landlord_rating": 3,
+        "tags_list": {
+          "noise": 3 
+        },
+        "message": "My apartment mates and I are happy with it. Pretty nice and quiet."
+      },
+      {
+        "apartment_name": "Collegetown Court",
+        "apartment_unit": "4 BD",
+        "overall_rating": 2,
+        "landlord_rating": 3,
+        "tags_list": { 
+          "price": 2 
+        },
+        "message": "It isn't really worth the price."
+      }
+    ]
+  },
+  {
+    "name": "Aces Apartments",
+    "location": "418 - 428 Eddy Street, Ithaca, NY 14850",
+    "landlord": "Ithaca Renting Company",
+    "image": "aces",
+    "overall_rating": 3,
+    "landlord_rating": 2,
+    "tags_list": {
+      "noise": 2,
+      "location": 3,
+      "amenities": 2
+    },
+    "reviews": [
+      {
+        "apartment_name": "Aces Apartments",
+        "apartment_unit": "1 BD",
+        "overall_rating": 4,
+        "landlord_rating": 3,
+        "tags_list": {
+          "noise": 2,
+          "location": 3 
+        },
+        "message": "Super close to campus and I've been liking my 1 BD so far. It can get a little noisy on Friday nights and the weekend because it's on a busy street so bear that in mind."
+      },
+      {
+        "apartment_name": "Aces Apartments",
+        "apartment_unit": "4 BD",
+        "overall_rating": 2,
+        "landlord_rating": 1,
+        "tags_list": { 
+          "noise": 1,
+          "location": 3,
+          "amenities": 2
+        },
+        "message": "DID NOT HAVE A GOOD EXPERIENCE :(. Super noisy!"
+      }
+    ]
+  }
+];
+
